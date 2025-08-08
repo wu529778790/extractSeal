581261d32bdd73c346881d7dc85493ae53dfa417
wu529778790 <529778790@qq.com>
2025-08-08 21:10:25 +0800

    重构：移除直接导入 OpenCV.js 的代码，改为通过 loadOpenCV 函数加载 OpenCV，更新相关接口以支持加载选项。

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from "vue";
 import Index from "./index.vue";
 
-// 不再直接导入 opencv.js
-// import "opencv.js";
-
 createApp(Index).mount("#app");
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,12 +2,18 @@ export interface ExtractOptions {
   color?: string;
 }
 
+export interface OpenCVLoadOptions {
+  url?: string;
+  timeoutMs?: number;
+}
+
 export default class StampExtractor {
   private cvReady: boolean;
   constructor();
-  initOpenCV(): Promise<boolean>;
+  initOpenCV(options?: OpenCVLoadOptions): Promise<boolean>;
   extractFromFile(file: File, options?: ExtractOptions): Promise<string[]>;
   extractFromImage(img: HTMLImageElement, options?: ExtractOptions): string[];
 }
 
 export { StampExtractor };
+export function loadOpenCV(options?: OpenCVLoadOptions): Promise<boolean>;
